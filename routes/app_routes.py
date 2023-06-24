1121b42d6f676ef93aaa336999016b9719814f98
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-06-24 13:34:14 -0400

    adding the route login and register to the page

--- routes/app_routes.py
+++ routes/app_routes.py
@@ -5,3 +5,11 @@ Routes = Blueprint("routes", __name__)
 @Routes.route("/")
 def home():
     return render_template("content.html")
+
+@Routes.route("/login")
+def login():
+    return render_template("login.html")
+
+@Routes.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
