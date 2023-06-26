d4c80e466a1095de04bd3b14e652985edbe98133
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-06-26 13:33:57 -0400

    fixing readibility config and add bcrypt config

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Flask
-from flask_mysqldb import MySQL, MySQLdb
+from flask_bcrypt import Bcrypt
 
-from config import _DB, _HOST, _PASSWORD, _SECRET_KEY, _USER
+from config import config_app
 from routes.app_routes import Routes
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
-app.config["MYSQL_HOST"] = _HOST
-app.config["MYSQL_USER"] = _USER
-app.config["MYSQL_PASSWORD"] = _PASSWORD
-app.config["MYSQL_DB"] = _DB
-app.config["SECRET_KEY"] = _SECRET_KEY
-
-mysql = MySQL(app)
-
-app.register_blueprint(Routes)
+config_app(app, Routes)
 
 if __name__ == "__main__":
     app.run(debug=True)
