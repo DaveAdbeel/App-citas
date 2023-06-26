021ab262e77dcb25d1cc5013f08a80f5415b68f2
DaveAdbeel <davidadbeelgonzalez@gmail.com>
2023-06-26 13:34:22 -0400

    adding readibility to the app wit classes

--- config.py
+++ config.py
@@ -9,3 +9,8 @@ _PASSWORD = os.getenv("MYSQL_PASSWORD")
 _HOST = os.getenv("MYSQL_HOST")
 _DB = os.getenv("MYSQL_DB")
 _SECRET_KEY = os.getenv("SECRET_KEY")
+
+class config_app():
+    def __init__(self, app, route):
+        app.config["SECRET_KEY"] = _SECRET_KEY
+        app.register_blueprint(route)
